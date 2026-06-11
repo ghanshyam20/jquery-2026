13b74b2bcd4ca3d2323de2f0fb54c899b6ec47ee
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-06-11 20:48:13 +0300

    added new feature

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -9,3 +9,29 @@ document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 });
 }
 
+
+// Detecting Button Press
+
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrumButtons; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonInnerHTML=this.innerHTML;
+    alert(buttonInnerHTML);
+});
+}
+
+// Detecting Keyboard Press
+
+document.addEventListener("keypress",function(event){
+    alert("A key was pressed!");
+});
+
+document.addEventListener("keypress",function(event){
+    alert(event.key);
+});
+
+
+
+
